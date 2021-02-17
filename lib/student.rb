3f3161f6ca8a7b2790b6f6040eeb0c05be7f70d5
4anth0ny1 <acatullo4@gmail.com>
2021-02-17 15:05:30 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -49,13 +49,23 @@ class Student
   end
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = self.new(id=nil, name, grade)
     new_student.id = row[0]
     new_student.name =  row[1]
     new_student.grade = row[2]
-    new_student  # return the newly created instance
+    new_student  
   end
+
+  def self.find_by_name(name) 
+    sql = <<-SQL
+        SELECT * 
+        FROM students
+        WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+    end
+  end   
 
   def update
     sql = <<-SQL
